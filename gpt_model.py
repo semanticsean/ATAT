45b14ee2c757943e8b837c5c1bd62e444b62f277
realityinspector <23709693-realityinspector@users.noreply.replit.com>
2023-09-21 22:41:35 +0000

    destructive restore to get detail working again

--- gpt_model.py
+++ gpt_model.py
@@ -6,134 +6,108 @@ import openai
 
 openai_api_key = os.environ['OPENAI_API_KEY']
 
+
 class GPTModel:
 
-    def __init__(self):
-        openai.api_key = openai_api_key
-        self.last_api_call_time = 0
-        print("GPTModel Initialized")
+  def __init__(self):
+    openai.api_key = openai_api_key
+    self.last_api_call_time = 0
 
-    def generate_response(self, dynamic_prompt, content, conversation_history, additional_context=None, note=None):
-        print("Generating Response from gpt-4 call")
+  def generate_response(self,
+                        dynamic_prompt,
+                        content,
+                        conversation_history,
+                        additional_context=None,
+                        note=None):
+    print("Generating Response from gpt-4 call")
+    full_content = f"{content}\n\n{conversation_history}"
+    if additional_context:
+      full_content += f"\n{additional_context}"
+    if note:
+      full_content += f"\n{note}"
 
-        current_time = time.time()
-        elapsed_time = current_time - self.last_api_call_time
+    current_time = time.time()
+    elapsed_time = current_time - self.last_api_call_time
 
-        # If it hasn't been 60 seconds since the last API call, wait for the remaining time
-        if elapsed_time < 60:
-            sleep_duration = 60 - elapsed_time
-            print(f"Sleeping for {sleep_duration:.2f} seconds to avoid rate limits.")
-            time.sleep(sleep_duration)
+    # If it hasn't been 60 seconds since the last API call, wait for the remaining time
+    if elapsed_time < 60:
+      sleep_duration = 60 - elapsed_time
+      print(f"Sleeping for {sleep_duration:.2f} seconds to avoid rate limits.")
+      time.sleep(sleep_duration)
 
-        while True:
-            full_content = f"{content}\n\n{conversation_history}"
-            if additional_context:
-                full_content += f"\n{additional_context}"
-            if note:
-                full_content += f"\n{note}"
+    # Calculate the maximum tokens allowed for the conversation content.
+    max_tokens_allowed = 4192 - len(content.split()) - len(dynamic_prompt.split())
+    if additional_context:
+      max_tokens_allowed -= len(additional_context.split())
+    if note:
+      max_tokens_allowed -= len(note.split())
 
-            max_tokens_allowed = 4192 - len(content.split()) - len(dynamic_prompt.split())
-            if additional_context:
-                max_tokens_allowed -= len(additional_context.split())
-            if note:
-                max_tokens_allowed -= len(note.split())
+    # Check and truncate conversation_history if total tokens exceed the maximum limit
+    while len(conversation_history.split()) > max_tokens_allowed:
+      # Drop the oldest message (assumes each message in the history is separated by a newline)
+      conversation_history = "\n".join(conversation_history.split("\n")[1:])
 
-            # If there's still room, break out of the loop
-            if len(full_content.split()) <= max_tokens_allowed:
-                break
+    # Update the full_content after potentially truncating the conversation_history
+    full_content = f"{content}\n\n{conversation_history}"
+    if additional_context:
+      full_content += f"\n{additional_context}"
+    if note:
+      full_content += f"\n{note}"
 
-            # Try to truncate conversation_history first
-            if len(conversation_history.split("\n")) > 1:
-                conversation_history = "\n".join(conversation_history.split("\n")[1:])
-                continue
+    response = None
+    max_retries = 99
+    delay = 60  # variable
+    max_delay = 3000  # variable
 
-            # If truncating conversation_history isn't enough, try to truncate additional_context
-            if additional_context and len(additional_context.split("\n")) > 1:
-                additional_context = "\n".join(additional_context.split("\n")[1:])
-                continue
+    for i in range(max_retries):
+      try:
+        request_payload = {
+            "model":
+            "gpt-4",
+            "messages": [{
+                "role": "system",
+                "content": dynamic_prompt
+            }, {
+                "role": "user",
+                "content": full_content
+            }],
+            "max_tokens":
+            4000,
+            "top_p":
+            0.7,
+            "frequency_penalty":
+            0.2,
+            "presence_penalty":
+            0.2,
+            "temperature":
+            0.6
+        }
 
-            # Finally, try to truncate note if needed
-            if note and len(note.split("\n")) > 1:
-                note = "\n".join(note.split("\n")[1:])
-                continue
+        print("\n--- API Request Payload ---")
+        print(json.dumps(request_payload, indent=4))
 
-            # If we reach here, it means we can't truncate further
-            print("Cannot truncate content further to fit API call.")
-            return "Error: Content is too long. Please shorten your input."
+        response = openai.ChatCompletion.create(
+            **request_payload)  # Updated this line
 
-        conversation_history = ""  # Clear conversation history to make space
+        print("\n--- API Response ---")
+        print(json.dumps(response, indent=4))
 
-        is_error_logged = False
+        break
 
-        while len(conversation_history.split()) > max_tokens_allowed:
-            if not is_error_logged:
-                print("Truncating conversation history...")
-                is_error_logged = True
-            # Drop half of the messages in the history
-            conversation_history = "\n".join(
-                conversation_history.split("\n")
-                [len(conversation_history.split("\n")) // 2:])
+      except openai.OpenAIError as e:
+        print(e)
+        sleep_time = max(
+            min(delay * (2**i) + uniform(0.0, 0.1 * (2**i)), max_delay), 30)
+        print(f"Retrying in {sleep_time:.2f} seconds.")
+        time.sleep(sleep_time)
+      else:
+        break
 
-        print("Finalizing content for API request.")
-        full_content = f"{content}\n\n{conversation_history}"
-        if additional_context:
-            full_content += f"\n{additional_context}"
-        if note:
-            full_content += f"\n{note}"
+    if response is None:
+      print("Max retries reached. Could not generate a response.")
+      return None
 
-        response = None
-        max_retries = 99
-        delay = 60  # variable
-        max_delay = 3000  # variable
+    # Update the last_api_call_time at the end of the API call
+    self.last_api_call_time = time.time()
 
-        for i in range(max_retries):
-            try:
-                if len(full_content.split()) > max_tokens_allowed:
-                    # Truncate from the beginning of the content
-                    full_content = " ".join(full_content.split()[-max_tokens_allowed:])
-
-                request_payload = {
-                    "model": "gpt-4",
-                    "messages": [{
-                                "role": "system",
-                                "content": dynamic_prompt
-                            }, {
-                                "role": "user",
-                                "content": full_content
-                            }],
-                            "max_tokens": 4000,
-                            "top_p": 0.7,
-                            "frequency_penalty": 0.2,
-                            "presence_penalty": 0.2,
-                            "temperature": 0.6
-                        }
-
-                print("\n--- API Request Payload ---")
-                print(json.dumps(request_payload, indent=4))
-
-                response = openai.ChatCompletion.create(
-                    **request_payload)  # Updated this line
-
-                print("\n--- API Response ---")
-                print(json.dumps(response, indent=4))
-
-                break
-
-            except openai.OpenAIError as e:
-                print(e)
-                sleep_time = max(
-                    min(delay * (2**i) + uniform(0.0, 0.1 * (2**i)), max_delay), 30)
-                print(f"Retrying in {sleep_time:.2f} seconds.")
-                time.sleep(sleep_time)
-            else:
-                break
-
-        if response is None:
-            print("Max retries reached. Could not generate a response.")
-            return None
-
-        # Update the last_api_call_time at the end of the API call
-        self.last_api_call_time = time.time()
-        print(f"Updated last API call time to: {self.last_api_call_time}")
-
-        return response['choices'][0]['message']['content']
+    return response['choices'][0]['message']['content']
--- shortcode.py
+++ shortcode.py
@@ -2,145 +2,120 @@ import json
 import re
 from doc_gen import gpt4_generate_structured_response
 
+
 def auto_split_content(content, char_limit=3000):
-    """
+  """
     Split the content based on character count and paragraph breaks.
     """
-    chunks = []
-    while content:
-        # If the content is under the char_limit, append it to chunks and break
-        if len(content) <= char_limit:
-            chunks.append(content)
-            break
+  chunks = []
+  while content:
+    # If the content is under the char_limit, append it to chunks and break
+    if len(content) <= char_limit:
+      chunks.append(content)
+      break
 
-        # Find the last paragraph break before the char_limit
-        split_idx = content.rfind('\n', 0, char_limit)
+    # Find the last paragraph break before the char_limit
+    split_idx = content.rfind('\n', 0, char_limit)
 
-        # If we didn't find a break, split at the char_limit
-        split_idx = split_idx if split_idx != -1 else char_limit
+    # If we didn't find a break, split at the char_limit
+    split_idx = split_idx if split_idx != -1 else char_limit
 
-        # Append the chunk to our list
-        chunks.append(content[:split_idx])
+    # Append the chunk to our list
+    chunks.append(content[:split_idx])
 
-        # Remove the chunked content
-        content = content[split_idx:].strip()
+    # Remove the chunked content
+    content = content[split_idx:].strip()
 
-    return chunks
+  return chunks
+
 
 def handle_document_short_code(email_content,
                                api_key,
                                conversation_history=None,
                                last_agent_response=""):
-    """
+  """
     This function handles both the !detail and !summarize shortcodes.
     """
-    # Initialize the result dictionary
+  # Initialize the result dictionary
+  result = {'type': None, 'content': None, 'new_content': email_content}
+
+  # Handling !detail short-code using unique delimiters
+  detail_matches = re.findall(r"!detail_start!(.*?)!detail_stop!",
+                              email_content, re.DOTALL)
+  if detail_matches:
+    detailed_responses = []
+    for match in detail_matches:
+      detail_content = match.strip()
+
+      # Check if the content has the !previousResponse keyword and replace it
+      if "!previousResponse" in detail_content:
+        detail_content = detail_content.replace('!previousResponse',
+                                                last_agent_response)
+
+      # Split content based on !split and treat each section as a separate chunk
+      split_sections = re.split(r'!split!', detail_content)
+      detailed_responses.extend(
+          [section.strip() for section in split_sections if section.strip()])
+
+    new_email_content = re.sub(r"!detail_start!(.*?)!detail_stop!",
+                               "",
+                               email_content,
+                               flags=re.DOTALL).strip()
+    result['type'] = 'detail'
+    result['content'] = detailed_responses
+    result['new_content'] = new_email_content
+    return result
+
+  # Handling !summarize short-code using unique delimiters
+  summarize_matches = re.findall(
+      r"!summarize\.(.*?)_start!(.*?)!summarize_stop!", email_content,
+      re.DOTALL)
+  if summarize_matches:
+    modality, summarize_content = summarize_matches[0]
+    summarize_content = summarize_content.strip()
+
+    # Check if the content has the !previousResponse keyword and replace it
+    if "!previousResponse" in summarize_content:
+      summarize_content = summarize_content.replace('!previousResponse',
+                                                    last_agent_response)
+
+    # Check if the summarize_content is too long and needs splitting
+    if len(summarize_content) > 3000:  # or any other limit you prefer
+      summarized_responses = auto_split_content(summarize_content)
+    else:
+      summarized_responses = [summarize_content]
+
+    new_email_content = re.sub(
+        r"!summarize\.(.*?)_start!(.*?)!summarize_stop!",
+        "",
+        email_content,
+        flags=re.DOTALL).strip()
+    result['type'] = 'summarize'
+    result['content'] = summarized_responses
+    result['modality'] = modality
+    result['new_content'] = new_email_content
+    return result
+
+  # Ensuring result is a dictionary before returning
+  if not isinstance(result, dict):
     result = {'type': None, 'content': None, 'new_content': email_content}
 
-    # Handling !detail short-code using unique delimiters
-    detail_matches = re.findall(r"!detail_start!(.*?)!detail_stop!", email_content, re.DOTALL)
-    if detail_matches:
-        detailed_responses = []
-        for match in detail_matches:
-            detail_content = match.strip()
-            
-            print(f"Matched !detail content: {detail_content[:100]}...")  # Debug: print the first 100 characters
-    
-            # Check if the content has the !previousResponse keyword and replace it
-            if "!previousResponse" in detail_content:
-                detail_content = detail_content.replace('!previousResponse', last_agent_response)
-    
-            # Split content based on !split and treat each section as a separate chunk
-            split_sections = re.split(r'!split!', detail_content)
-            
-            for section in split_sections:  # Debug: print each split section
-                print(f"Split section (first 100 characters): {section.strip()[:100]}...")
-    
-            detailed_responses.extend([section.strip() for section in split_sections if section.strip()])
-    
-        # Rest of the code...
-    
+  # Ensuring result is a dictionary before returning
+  return {'type': None, 'content': None, 'new_content': email_content}
 
-        new_email_content = re.sub(r"!detail_start!(.*?)!detail_stop!",
-                                   "",
-                                   email_content,
-                                   flags=re.DOTALL).strip()
-        result['type'] = 'detail'
-        result['content'] = detailed_responses
-        result['new_content'] = new_email_content
-        return result
 
-    # Handling !summarize short-code using unique delimiters
-# Initialize modality to None
-    modality = None
-
-    # Handling !summarize short-code using unique delimiters
-    summarize_matches = re.findall(
-        r"!summarize\.(.*?)_start!(.*?)!summarize_stop!", email_content,
-        re.DOTALL)
-    
-    print("=== Inside handle_document_short_code function ===")
-    print(f"Initial email content: {email_content[:100]}...")  # Print the first 100 characters
-
-    if summarize_matches:
-        modality, summarize_content = summarize_matches[0] # Unpack the matched groups
-        summarize_content = summarize_content.strip()
-
-        # Check if the content has the !previousResponse keyword and replace it
-        if "!previousResponse" in summarize_content:
-            summarize_content = summarize_content.replace('!previousResponse',
-                                                          last_agent_response)
-
-        # Split the summarize_content into chunks
-        summarized_responses = split_content_into_chunks(summarize_content)
-
-        # Debug prints for summarization
-        print("Recognized !summarize shortcode")
-        print(f"Modality: {modality}")
-        print(f"Content to summarize: {summarize_content[:100]}...")  # First 100 characters
-        print(f"Number of chunks: {len(summarized_responses)}")
-
-        new_email_content = re.sub(
-            r"!summarize\.(.*?)_start!(.*?)!summarize_stop!",
-            "",
-            email_content,
-            flags=re.DOTALL).strip()
-        result['type'] = 'summarize'
-        result['content'] = summarized_responses
-        result['modality'] = modality
-        result['new_content'] = new_email_content
-        return result
-
-    # If no shortcode matches were found, return a default result
-    return {'type': None, 'content': None, 'new_content': email_content}
-
-    # Ensuring result is a dictionary before returning
-    if not isinstance(result, dict):
-        result = {'type': None, 'content': None, 'new_content': email_content}
-
-    # Ensuring result is a dictionary before returning
-    return {'type': None, 'content': None, 'new_content': email_content}
-
-def split_content_into_chunks(content, max_char_count=5000):
-    chunks = []
-    # Split the content into sentences
-    sentences = re.split('(?<=[.!?]) +', content.strip())
-    current_chunk = ""
-    for sentence in sentences:
-        if len(current_chunk) + len(sentence) + 1 <= max_char_count:
-            current_chunk += (" " + sentence).strip()
-        else:
-            chunks.append(current_chunk.strip())
-            current_chunk = sentence.strip()
-    
-    print("=== Inside split_content_into_chunks function ===")
-    print(f"Number of sentences: {len(sentences)}")
-    print(f"Number of chunks: {len(chunks)}")
-    
-    # After processing the chunks, add any necessary debug prints or processing steps
-    
-    # After processing all sentences, if current_chunk is not empty, add it as a chunk
-    if current_chunk:
-        chunks.append(current_chunk.strip())
-
-    return chunks
+def split_content_into_chunks(content, max_char_count=9000):
+  chunks = []
+  # Split the content into sentences
+  sentences = re.split('(?<=[.!?]) +', content.strip())
+  current_chunk = ""
+  for sentence in sentences:
+    if len(current_chunk) + len(sentence) + 1 <= max_char_count:
+      current_chunk += (" " + sentence).strip()
+    else:
+      chunks.append(current_chunk.strip())
+      current_chunk = sentence.strip()
+  # After processing all sentences, if current_chunk is not empty, add it as a chunk
+  if current_chunk:
+    chunks.append(current_chunk.strip())
+  return chunks
