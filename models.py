f1128842187bb1376e94d042d7e829d545ab0131
semanticsean <140459476+semanticsean@users.noreply.github.com>
2024-03-11 23:28:18 +0000

    login accepts email

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256))
     folder_path = db.Column(db.String(128), unique=True)
 
